780111cc2f1f03708cdca575d2814d69e92a2eab
samjesus8 <98812930+samjesus8@users.noreply.github.com>
2022-10-18 17:24:02 +0100

    Base Program
    -Fixed Form loading Issues

--- QuizzApp/Main.Designer.cs
+++ QuizzApp/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuizzApp
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
