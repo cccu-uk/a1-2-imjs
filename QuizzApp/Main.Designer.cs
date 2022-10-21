a65bbb67eaf9d67334475a1c0d59f32d4cab995c
Megan McKnight <mm1286@canterbury.ac.uk>
2022-10-21 15:32:28 +0100

    Added quiz form with questions and answers

--- QuizzApp/Main.Designer.cs
+++ QuizzApp/Main.Designer.cs
@@ -35,10 +35,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(69, 48);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(60, 36);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 72);
+            this.button1.Size = new System.Drawing.Size(103, 54);
             this.button1.TabIndex = 0;
             this.button1.Text = "Single Player";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,10 +45,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(69, 128);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Location = new System.Drawing.Point(60, 96);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(118, 71);
+            this.button2.Size = new System.Drawing.Size(103, 53);
             this.button2.TabIndex = 1;
             this.button2.Text = "Two Player";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,14 +55,13 @@
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Main";
             this.Text = "Form1";
             this.ResumeLayout(false);
---
+++ QuizzApp/QuizForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace QuizzApp
+{
+    partial class QuizForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelQuestionNum = new System.Windows.Forms.Label();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.buttonAnswerOne = new System.Windows.Forms.Button();
+            this.buttonAnswerTwo = new System.Windows.Forms.Button();
+            this.buttonAnswerThree = new System.Windows.Forms.Button();
+            this.buttonAnswerFour = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelQuestionNum
+            // 
+            this.labelQuestionNum.AutoSize = true;
+            this.labelQuestionNum.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelQuestionNum.Location = new System.Drawing.Point(30, 18);
+            this.labelQuestionNum.Name = "labelQuestionNum";
+            this.labelQuestionNum.Size = new System.Drawing.Size(30, 25);
+            this.labelQuestionNum.TabIndex = 0;
+            this.labelQuestionNum.Text = "Q.";
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Location = new System.Drawing.Point(30, 56);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(38, 15);
+            this.labelQuestion.TabIndex = 1;
+            this.labelQuestion.Text = "label1";
+            // 
+            // buttonAnswerOne
+            // 
+            this.buttonAnswerOne.Location = new System.Drawing.Point(39, 97);
+            this.buttonAnswerOne.Name = "buttonAnswerOne";
+            this.buttonAnswerOne.Size = new System.Drawing.Size(283, 139);
+            this.buttonAnswerOne.TabIndex = 2;
+            this.buttonAnswerOne.Text = "button1";
+            this.buttonAnswerOne.UseVisualStyleBackColor = true;
+            // 
+            // buttonAnswerTwo
+            // 
+            this.buttonAnswerTwo.Location = new System.Drawing.Point(347, 97);
+            this.buttonAnswerTwo.Name = "buttonAnswerTwo";
+            this.buttonAnswerTwo.Size = new System.Drawing.Size(283, 139);
+            this.buttonAnswerTwo.TabIndex = 3;
+            this.buttonAnswerTwo.Text = "button2";
+            this.buttonAnswerTwo.UseVisualStyleBackColor = true;
+            // 
+            // buttonAnswerThree
+            // 
+            this.buttonAnswerThree.Location = new System.Drawing.Point(39, 258);
+            this.buttonAnswerThree.Name = "buttonAnswerThree";
+            this.buttonAnswerThree.Size = new System.Drawing.Size(283, 139);
+            this.buttonAnswerThree.TabIndex = 4;
+            this.buttonAnswerThree.Text = "button3";
+            this.buttonAnswerThree.UseVisualStyleBackColor = true;
+            // 
+            // buttonAnswerFour
+            // 
+            this.buttonAnswerFour.Location = new System.Drawing.Point(347, 258);
+            this.buttonAnswerFour.Name = "buttonAnswerFour";
+            this.buttonAnswerFour.Size = new System.Drawing.Size(283, 139);
+            this.buttonAnswerFour.TabIndex = 5;
+            this.buttonAnswerFour.Text = "button4";
+            this.buttonAnswerFour.UseVisualStyleBackColor = true;
+            // 
+            // QuizForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 450);
+            this.Controls.Add(this.buttonAnswerFour);
+            this.Controls.Add(this.buttonAnswerThree);
+            this.Controls.Add(this.buttonAnswerTwo);
+            this.Controls.Add(this.buttonAnswerOne);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.labelQuestionNum);
+            this.Name = "QuizForm";
+            this.Text = "QuizForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelQuestionNum;
+        private Label labelQuestion;
+        private Button buttonAnswerOne;
+        private Button buttonAnswerTwo;
+        private Button buttonAnswerThree;
+        private Button buttonAnswerFour;
+    }
+}
